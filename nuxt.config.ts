149c6339456596432f003a26300224232a363e24
夏沫花火zzz🌙 <xmcn@1l1.icu>
2025-05-07 00:35:03 +0800

    Fix target

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   ssr: false,
+  target: "static",
   modules: [
     "@nuxt/eslint",
     "nuxtjs-naive-ui",

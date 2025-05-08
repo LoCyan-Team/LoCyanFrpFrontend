e0d74a73212e30f2af470a5da23ddaf7e4ae696d
夏沫花火zzz🌙 <xmcn@1l1.icu>
2025-05-09 00:23:16 +0800

    Add vertical space for pagination

--- pages/tunnel/manage.vue
+++ pages/tunnel/manage.vue
@@ -37,156 +37,162 @@
       </transition>
 
       <n-spin :show="loading.page">
-        <n-empty v-if="tunnels.length === 0" />
-        <n-el v-else>
-          <!-- 卡片模式 -->
-          <n-el v-if="viewMode === 'card'">
-            <n-grid :y-gap="8" :x-gap="12" :cols="4" item-responsive>
-              <n-grid-item
-                v-for="tunnel in tunnels"
-                :key="tunnel.id"
-                span="0:4 1000:1"
-              >
-                <n-spin
-                  :show="tunnel.status === -1"
-                  style="
-                    --disable-color: #d90303;
-                    --n-color: var(--disable-color);
-                    --n-text-color: var(--disable-color);
-                  "
-                  :rotate="false"
+        <n-space vertical>
+          <n-empty v-if="tunnels.length === 0" />
+          <n-el v-else>
+            <!-- 卡片模式 -->
+            <n-el v-if="viewMode === 'card'">
+              <n-grid :y-gap="8" :x-gap="12" :cols="4" item-responsive>
+                <n-grid-item
+                  v-for="tunnel in tunnels"
+                  :key="tunnel.id"
+                  span="0:4 1000:1"
                 >
-                  <template #icon>
-                    <n-icon :component="Error" />
-                  </template>
-                  <template #description>已封禁</template>
-                  <n-card :title="tunnel.name">
-                    <template #header-extra>
-                      <n-space style="margin-right: 10px">
-                        <n-tag type="success">
-                          {{ tunnel.type.toUpperCase() }}
-                        </n-tag>
-                        <n-tag type="info"> ID: {{ tunnel.id }} </n-tag>
-                      </n-space>
-                      <n-el @click="handleBatchSelect(tunnel.id)">
-                        <n-checkbox
-                          :checked="batchSelected.includes(tunnel.id)"
-                          size="large"
-                        />
-                      </n-el>
+                  <n-spin
+                    :show="tunnel.status === -1"
+                    style="
+                      --disable-color: #d90303;
+                      --n-color: var(--disable-color);
+                      --n-text-color: var(--disable-color);
+                    "
+                    :rotate="false"
+                  >
+                    <template #icon>
+                      <n-icon :component="Error" />
                     </template>
-                    <!-- TODO: Content -->
-                    <n-text>节点: {{ tunnel.node.name ?? "未知节点" }}</n-text>
-                    <br /><n-text>本地端口: {{ tunnel.localPort }}</n-text>
-                    <br /><n-text
-                      >远程端口: {{ tunnel.remotePort ?? "无" }}</n-text
-                    >
-                    <br /><n-text
-                      >自定义域名: {{ tunnel.domain ?? "无" }}</n-text
-                    >
-                    <template v-if="tunnel.node.host" #footer>
-                      <n-text>连接地址:</n-text>
-                      <n-scrollbar x-scrollable>
-                        <n-code code="" />
-                      </n-scrollbar>
-                    </template>
-                    <template #action>
-                      <n-space>
-                        <n-button type="info" secondary>详细信息</n-button>
-                        <n-button type="success" secondary>修改</n-button>
-                        <n-button type="error" secondary>删除</n-button>
-                        <n-button type="warning" secondary>强制下线</n-button>
-                      </n-space>
-                    </template>
-                  </n-card>
-                </n-spin>
-              </n-grid-item>
-            </n-grid>
-          </n-el>
-          <!-- 列表模式 -->
-          <n-el v-if="viewMode === 'list'">
-            <n-scrollbar x-scrollable>
-              <n-table
-                style="min-width: 800px"
-                :bordered="true"
-                :single-line="false"
-              >
-                <n-thead>
-                  <n-tr>
-                    <n-th>选择</n-th>
-                    <n-th>隧道 ID</n-th>
-                    <n-th>隧道名</n-th>
-                    <n-th>隧道节点</n-th>
-                    <n-th>隧道协议</n-th>
-                    <n-th>本地端口</n-th>
-                    <n-th>远程端口</n-th>
-                    <n-th>自定义域名</n-th>
-                    <n-th>连接地址</n-th>
-                    <n-th>操作</n-th>
-                  </n-tr>
-                </n-thead>
-                <n-tbody>
-                  <n-tr v-for="tunnel in tunnels" :key="tunnel.id">
-                    <n-td>
-                      <n-el @click="handleBatchSelect(tunnel.id)">
-                        <n-checkbox
-                          :checked="batchSelected.includes(tunnel.id)"
-                        />
-                      </n-el>
-                    </n-td>
-                    <n-td>{{ tunnel.id }}</n-td>
-                    <n-td>{{ tunnel.name }}</n-td>
-                    <n-td>
-                      {{ tunnel.node.name ?? "未知节点" }}
-                    </n-td>
-                    <n-td>{{ tunnel.type.toUpperCase() }}</n-td>
-                    <n-td>{{ tunnel.localPort }}</n-td>
-                    <n-td>{{ tunnel.remotePort }}</n-td>
-                    <n-td>{{ tunnel.domain }}</n-td>
-                    <n-td v-if="tunnel.node.host">
-                      <!-- TODO: Compute connect address -->
-                    </n-td>
-                    <n-td v-else />
-                    <n-td>
-                      <n-spin
-                        :show="tunnel.status === -1"
-                        style="
-                          --disable-color: #d90303;
-                          --n-color: var(--disable-color);
-                          --n-text-color: var(--disable-color);
-                        "
-                        :rotate="false"
+                    <template #description>已封禁</template>
+                    <n-card :title="tunnel.name">
+                      <template #header-extra>
+                        <n-space style="margin-right: 10px">
+                          <n-tag type="success">
+                            {{ tunnel.type.toUpperCase() }}
+                          </n-tag>
+                          <n-tag type="info"> ID: {{ tunnel.id }} </n-tag>
+                        </n-space>
+                        <n-el @click="handleBatchSelect(tunnel.id)">
+                          <n-checkbox
+                            :checked="batchSelected.includes(tunnel.id)"
+                            size="large"
+                          />
+                        </n-el>
+                      </template>
+                      <!-- TODO: Content -->
+                      <n-text
+                        >节点: {{ tunnel.node.name ?? "未知节点" }}</n-text
                       >
-                        <template #icon>
-                          <n-icon :component="Error" />
-                        </template>
-                        <template #description>已封禁</template>
-                        <n-space :inline="true" :wrap="false">
+                      <br /><n-text>本地端口: {{ tunnel.localPort }}</n-text>
+                      <br /><n-text
+                        >远程端口: {{ tunnel.remotePort ?? "无" }}</n-text
+                      >
+                      <br /><n-text
+                        >自定义域名: {{ tunnel.domain ?? "无" }}</n-text
+                      >
+                      <template v-if="tunnel.node.host" #footer>
+                        <n-text>连接地址:</n-text>
+                        <n-scrollbar x-scrollable>
+                          <n-code code="" />
+                        </n-scrollbar>
+                      </template>
+                      <template #action>
+                        <n-space>
                           <n-button type="info" secondary>详细信息</n-button>
                           <n-button type="success" secondary>修改</n-button>
                           <n-button type="error" secondary>删除</n-button>
                           <n-button type="warning" secondary>强制下线</n-button>
                         </n-space>
-                      </n-spin>
-                    </n-td>
-                  </n-tr>
-                </n-tbody>
-              </n-table>
-            </n-scrollbar>
+                      </template>
+                    </n-card>
+                  </n-spin>
+                </n-grid-item>
+              </n-grid>
+            </n-el>
+            <!-- 列表模式 -->
+            <n-el v-if="viewMode === 'list'">
+              <n-scrollbar x-scrollable>
+                <n-table
+                  style="min-width: 800px"
+                  :bordered="true"
+                  :single-line="false"
+                >
+                  <n-thead>
+                    <n-tr>
+                      <n-th>选择</n-th>
+                      <n-th>隧道 ID</n-th>
+                      <n-th>隧道名</n-th>
+                      <n-th>隧道节点</n-th>
+                      <n-th>隧道协议</n-th>
+                      <n-th>本地端口</n-th>
+                      <n-th>远程端口</n-th>
+                      <n-th>自定义域名</n-th>
+                      <n-th>连接地址</n-th>
+                      <n-th>操作</n-th>
+                    </n-tr>
+                  </n-thead>
+                  <n-tbody>
+                    <n-tr v-for="tunnel in tunnels" :key="tunnel.id">
+                      <n-td>
+                        <n-el @click="handleBatchSelect(tunnel.id)">
+                          <n-checkbox
+                            :checked="batchSelected.includes(tunnel.id)"
+                          />
+                        </n-el>
+                      </n-td>
+                      <n-td>{{ tunnel.id }}</n-td>
+                      <n-td>{{ tunnel.name }}</n-td>
+                      <n-td>
+                        {{ tunnel.node.name ?? "未知节点" }}
+                      </n-td>
+                      <n-td>{{ tunnel.type.toUpperCase() }}</n-td>
+                      <n-td>{{ tunnel.localPort }}</n-td>
+                      <n-td>{{ tunnel.remotePort }}</n-td>
+                      <n-td>{{ tunnel.domain }}</n-td>
+                      <n-td v-if="tunnel.node.host">
+                        <!-- TODO: Compute connect address -->
+                      </n-td>
+                      <n-td v-else />
+                      <n-td>
+                        <n-spin
+                          :show="tunnel.status === -1"
+                          style="
+                            --disable-color: #d90303;
+                            --n-color: var(--disable-color);
+                            --n-text-color: var(--disable-color);
+                          "
+                          :rotate="false"
+                        >
+                          <template #icon>
+                            <n-icon :component="Error" />
+                          </template>
+                          <template #description>已封禁</template>
+                          <n-space :inline="true" :wrap="false">
+                            <n-button type="info" secondary>详细信息</n-button>
+                            <n-button type="success" secondary>修改</n-button>
+                            <n-button type="error" secondary>删除</n-button>
+                            <n-button type="warning" secondary
+                              >强制下线</n-button
+                            >
+                          </n-space>
+                        </n-spin>
+                      </n-td>
+                    </n-tr>
+                  </n-tbody>
+                </n-table>
+              </n-scrollbar>
+            </n-el>
           </n-el>
-        </n-el>
-        <n-space
-          v-if="tunnels.length !== 0"
-          justify="center"
-          style="width: 100%"
-        >
-          <n-pagination
-            v-model:page="page.current"
-            v-model:page-size="page.size"
-            :page-count="page.count"
-            show-size-picker
-            :page-sizes="[10, 25, 50, 100]"
-          />
+          <n-space
+            v-if="tunnels.length !== 0"
+            justify="center"
+            style="width: 100%"
+          >
+            <n-pagination
+              v-model:page="page.current"
+              v-model:page-size="page.size"
+              :page-count="page.count"
+              show-size-picker
+              :page-sizes="[10, 25, 50, 100]"
+            />
+          </n-space>
         </n-space>
       </n-spin>
     </n-space>

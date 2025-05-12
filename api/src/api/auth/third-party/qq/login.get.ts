816306633616cfcb20f4ad2a957485efad7a3a04
梁杰渝 <1624629689@qq.com>
2025-05-12 14:23:45 +0800

    create auth/third-party API

---
+++ api/src/api/auth/third-party/qq/login.get.ts
@@ -0,0 +1,8 @@
+import { API } from "../../../../type/api";
+import { Method } from "../../../../type/method"; 
+
+export class GetQQLogin extends API {
+  constructor() {
+    super("/auth/third-party/qq/login", Method.GET, {}, {});
+  }
+}

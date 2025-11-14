fc3f1c5a11623283b700ee11dc0aa5ca44b631c5
夏沫花火zzz <xmcn@1l1.icu>
2025-11-14 09:44:42 +0800

    Change NuxtLayout to nuxt-layout in error.vue

--- app/error.vue
+++ app/error.vue
@@ -1,5 +1,5 @@
 <template>
-  <NuxtLayout>
+  <nuxt-layout>
     <n-el v-if="error.statusCode === 404">
       <n-el class="flex-center outbox">
         <n-empty description="这里什么也找不到啊 QwQ" />
@@ -33,12 +33,12 @@
         </n-space>
       </n-el>
     </n-el>
-  </NuxtLayout>
+  </nuxt-layout>
 </template>
 
 <script setup lang="ts">
 import type { NuxtError } from "#app";
-
+  
 defineProps({
   // eslint-disable-next-line vue/require-default-prop
   error: Object as () => NuxtError,

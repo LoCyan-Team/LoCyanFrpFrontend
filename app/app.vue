c6919aa1ffc3b8c27982398126d3b88f897e8fc8
夏沫花火zzz <xmcn@1l1.icu>
2025-11-29 21:34:51 +0800

    Update fallback template in app.vue

--- app/app.vue
+++ app/app.vue
@@ -3,7 +3,7 @@
   <nuxt-layout>
     <client-only>
       <nuxt-page style="min-height: calc(100dvh - 61px)" />
-      <template>
+      <template #fallback>
         <n-skeleton text :repeat="2" />
         <n-skeleton text style="width: 60%" />
       </template>

80389f810ee5570fef0a123ee288174834538008
夏沫花火zzz <xmcn@1l1.icu>
2025-11-29 21:34:24 +0800

    Add skeleton loading template to app.vue

--- app/app.vue
+++ app/app.vue
@@ -3,6 +3,10 @@
   <nuxt-layout>
     <client-only>
       <nuxt-page style="min-height: calc(100dvh - 61px)" />
+      <template>
+        <n-skeleton text :repeat="2" />
+        <n-skeleton text style="width: 60%" />
+      </template>
     </client-only>
   </nuxt-layout>
 </template>
